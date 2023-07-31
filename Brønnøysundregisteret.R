55eb792ecbf239eb50ad328441d9b7e337006a11
Vilde Kvamme <vildekvamme@Vildes-MacBook-Air.local>
2023-07-31 11:19:26 +0200

    Ny versjon

--- Brønnøysundregisteret.R
+++ Brønnøysundregisteret.R
@@ -54,6 +54,36 @@ transport_enheter_alle <- transport_enheter_alle %>%
             `Registrert i Foretaksregisteret`, `Registrert i Frivillighetsregisteret`, `Registrert i Stiftelsesregisteret`,
             Konkurs, `Under avvikling`, `Under tvangsavvikling eller tvangsoppløsning`, Målform))
 
+# endre på variabelnavnene slik at de blir lettere å jobbe med
+transport_enheter_alle <- transport_enheter_alle %>% 
+  rename(organisasjonsnummer = Organisasjonsnummer,
+         navn = Navn, 
+         organisasjonsform_kode = Organisasjonsform.kode,
+         organisasjonsform_beskrivelse = Organisasjonsform.beskrivelse,
+         naeringskode = `Næringskode 1`, 
+         naeringskode_beskrivelse = `Næringskode 1.beskrivelse`, 
+         hjemmeside = Hjemmeside, 
+         institusjonell_sektorkode = `Institusjonell sektorkode`, 
+         institusjonell_sektorkode_beskrivelse = `Institusjonell sektorkode.beskrivelse`,
+         overordnet_enhet = `Overordnet enhet i offentlig sektor`)
+
+tran_enheter_narm <- transport_enheter_alle
+
+tran_enheter_narm$overordnet_enhet <- as.numeric(tran_enheter_narm$overordnet_enhet)
+tran_enheter_narm$institusjonell_sektorkode <- as.numeric(tran_enheter_narm$institusjonell_sektorkode)
+
+# Assuming 'tran_enheter_narm' is your dataset
+# Replace NA values with 0 in numeric columns
+numeric_cols <- sapply(tran_enheter_narm, is.numeric)
+tran_enheter_narm[, numeric_cols][is.na(tran_enheter_narm[, numeric_cols])] <- 0
+
+# Replace NA values with "NA" in character columns
+character_cols <- sapply(tran_enheter_narm, is.character)
+tran_enheter_narm[, character_cols][is.na(tran_enheter_narm[, character_cols])] <- "NA"
+
+
 #### lagre datasettet som en egen fil ####
 write.xlsx(transport_enheter_alle, "data/transport_enheter_alle.xlsx")
+
+write.xlsx(tran_enheter_narm, "data/tran_enheter_narm.xlsx")
 
